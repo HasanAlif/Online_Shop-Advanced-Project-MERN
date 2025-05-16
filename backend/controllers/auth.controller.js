ae1cc52deaff91d98f6e743a268aa498c33bba1c
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-17 01:32:01 +0600

    Backend Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Signup Auth route is working");
+}
+
+export const login = async (req, res) => {
+  res.send("login Auth route is working");
+}
+
+export const logout = async (req, res) => {
+  res.send("logout Auth route is working");
+}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
 
 const app = express();
 
+app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server is running on port '+ PORT);
 });
