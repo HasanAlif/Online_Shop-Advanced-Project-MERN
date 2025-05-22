692e3821cce345e7bf92ac7d8b7d8e87fb304631
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-22 20:47:21 +0600

    Cloudinary Setup & createProduct Route Controller

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,7 +1,7 @@
-import Redis from "ioredis"
-import dotenv from "dotenv"
-dotenv.config();// Load environment variables from .env file
-// This allows us to use environment variables in our code
+import Redis from "ioredis";
+import dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
+// This allows us to use environment variables in our codenpm install ioredis
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);// Create a new Redis client using the Upstash Redis URL from environment variables
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL); // Create a new Redis client using the Upstash Redis URL from environment variables
 // The Redis client is used to interact with the Redis database
