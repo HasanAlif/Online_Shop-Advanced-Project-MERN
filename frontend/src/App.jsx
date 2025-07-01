b16b39a6d3fdfa48e22ac7593fcbe0b99840c187
HasanAlif <alifalmehedihasan@gmail.com>
2025-07-01 18:25:17 +0600

    Navbar Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-      </Routes>
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      {/* Background gradient */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(34,211,238,0.4)_0%,rgba(59,130,246,0.3)_40%,rgba(30,41,59,0.2)_100%)]" />
+        </div>
+      </div>
+
+      <div className="relative z-50 pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
