a866c949c61a8ac74c7ff95fbbee71132e214b54
HasanAlif <alifalmehedihasan@gmail.com>
2025-07-02 00:13:55 +0600

    Authentication Functionalities for Frontend to Backend

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
-
+import { useUserStore } from "../stores/useUserStore";
 
 const SignUpPage = () => {
   const loading = false;
@@ -13,11 +13,11 @@ const SignUpPage = () => {
 		confirmPassword: "",
 	});
 
-	
+	const { signup, user } = useUserStore();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(formData);
+		signup(formData);
 	};
 
 	return (
